21f5b2e8d7ad5ac7d7b637c78bfd519bb99cdbdb
guptasanidhya <guptasanidhya6@gmail.com>
2023-11-29 10:31:13 +0530

    done with crud operations over company for a specific user

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -1,17 +1,46 @@
 #separate controller for api end points, all api end points will be inherited by api controller
 class Api::V1::CompaniesController < ApiController
 
-    before_action :set_company, only: [:show]
+    before_action :set_company, only: [:show, :update, :destroy ]
 
     def index 
         # @companies=Company.all
         @companies = current_user.companies
+        render json: @companies,status: :ok
     end
 
     def show 
         render json: @company, status: :ok
     end
 
+    def create 
+        #@company =Company.new(company_params)
+            @company =current_user.companies.new(company_params)
+            if @company.save
+                render json: @company, status: :ok
+            else
+                render json: {data: @company.errors.full_messages,status: "failed"},
+                status: :unprocessable_entity
+            end
+    end
+
+    def update 
+        if @company.update(company_params)
+            render json: @company,status: :ok
+        else 
+            render json: {data: @company.errors.full_messages, status: "failed"},
+            status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        if @company.destroy
+            render json: {data:'company deleted successfully', status: 'success'},
+            status: :ok
+        else
+            render json:{ data: 'something went wrong',status: "failed"}
+        end
+    end
 
     private
 
@@ -21,4 +50,9 @@ class Api::V1::CompaniesController < ApiController
     rescue ActiveRecord::RecordNotFound => error
         render json: error.message,status: :unauthorized
     end
+
+    def company_params
+        params.require(:company).permit(:name,:address,:established_year,:user_id)
+    end
+
 end
